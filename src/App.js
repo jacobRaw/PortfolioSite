36abbb77dd0a1e09288c7bf7c534ef4143c02dac
Jacob Rawung <jacob.rawung@student.unsw.edu.au>
2025-01-05 20:22:40 +1100

    Created navbar and implemented routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './pages/Home.jsx';
+import Projects from './pages/Projects.jsx';
+import NotFoundPage from './pages/NotFoundPage.jsx';
+import About from './pages/About.jsx';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React with the first message/feature another feature
-        </a>
-      </header>
-    </div>
+    <>
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/about" element={<About/>}/>
+          <Route path="*" element={<NotFoundPage/>}/>
+        </Routes>
+      </HashRouter>
+    </>
   );
 }
 
